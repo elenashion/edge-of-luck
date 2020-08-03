c4e7e8eadefe1d8278cf1d3438b6235afeceb6f5
elenashion <edgir@bk.ru>
2020-08-03 13:12:48 +0300

    EOL-02 - add test properties and test connection

--- src/test/java/edge.of.luck/AbstractTest.java
+++ src/test/java/edge.of.luck/AbstractTest.java
@@ -5,13 +5,15 @@ import edge.of.luck.classes.GameLogic;
 import edge.of.luck.classes.LogicManager;
 
 import java.io.FileInputStream;
-import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.Properties;
 
 public class AbstractTest {
     protected UsersHelper usersHelper;
     protected LogicManager logicManager;
     protected GameLogic gameLogic;
+    protected Connection connection;
 
     public AbstractTest() {
         try {
@@ -20,6 +22,11 @@ public class AbstractTest {
             logicManager = new LogicManager(properties);
             usersHelper = new UsersHelper(logicManager);
             gameLogic = logicManager.gameLogic;
-        } catch (IOException e) {}
+
+            String url = properties.getProperty("hibernate.db.url");
+            String user = properties.getProperty("hibernate.db.user");
+            String password = properties.getProperty("hibernate.db.password");
+            this.connection = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {}
     }
 }
